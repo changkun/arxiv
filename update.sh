913127716a928294cc02c3d6f3383fb4d4afac7a
Changkun Ou <hi@changkun.de>
2021-04-17 21:26:48 +0200

    server: use docker-compose and add update script

---
+++ update.sh
@@ -0,0 +1,7 @@
+python fetch_papers.py --search-query cat:cs.HC
+python download_pdfs.py
+python parse_pdf_to_text.py
+python thumb_pdf.py
+python analyze.py
+python buildsvm.py
+python make_cache.py
